7aaca10d33879ae934b6eb3ce1381a03e3d88698
skyleryangff <fy2330@columbia.edu>
2026-03-06 11:23:22 -0500

    Update YFinance_File.py

--- YFinance_File.py
+++ YFinance_File.py
@@ -1,13 +1,31 @@
 import yfinance as yf
 import pandas as pd
 
-def YahooData2returns(YahooData=None,symbol='AAPL'):
-    # Input:
-    # YahooData = data from Yahoo Finance
-    # Output:
-    # returns = array of returns
-    # Steps:
-    # Extract 'Close' and symbol (This is a 2d column. Demo below.)
-    # Calculate and return the lagged returns
-    returns = np.array([0.01      , 0.00990099])
+def YahooData2returns(YahooData):
+
+    prices = YahooData['Adj Close']
+    pricevec = prices.values
+    returns = pricevec[1:] / pricevec[:-1] - 1
     return returns
+
+def get_stock_data(symbol):
+    data = yf.download(symbol)
+    prices = data['Adj Close']
+    return prices
+
+prices = get_stock_data('GS')
+print(type(prices))
+pricevec = prices.values
+
+
+n = len(pricevec)
+ratiovec = pricevec[1:n] / pricevec[:n-1]
+
+
+def get_returns(pricevec):
+    returns = pricevec[1:] / pricevec[:-1] - 1
+    return returns
+
+
+returns = get_returns(pricevec)
+print(returns)
